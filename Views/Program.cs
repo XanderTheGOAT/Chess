1243d66c29ff624eb5aae91fe78ad0ac874db77d
Alexander Quintanilla <xander.quin15@gmail.com>
2018-07-31 10:59:39 -0600

    This adds the chess code body

---
+++ Views/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void FileRead(string path)
+        {
+            try
+            {
+                FileStream stream = new FileStream(path, FileMode.Open);
+                BinaryFormatter bf = new BinaryFormatter();
+                try
+                {
+
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine(path + " has been loaded");
+                }
+                stream.Close();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File doesn't exist \n\n");
+            }
+            catch (SerializationException)
+            {
+                Console.WriteLine("Failed to Load \n\n");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("There was an error with your path \n\n");
+            }
+        }
+    }
+}
