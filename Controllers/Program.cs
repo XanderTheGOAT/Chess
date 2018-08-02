034af698587276eb1cd097740ed3698d00547e53
Man1a <benjermaniac@yahoo.com>
2018-08-02 14:01:53 -0600

    Make sure to work in a separate branch from now on

--- Controllers/Program.cs
+++ Controllers/Program.cs
@@ -6,30 +6,47 @@ using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using Chess.Models;
 
 namespace Chess
 {
     class Program
     {
+        static ChessCoordinates[,] board = new ChessCoordinates[8, 8];
+
         static void Main(string[] args)
         {
             string fileMessage = "";
-            //FileRead("C:\\Neumont Apps\\Visual Studio\\Project Course\\ChessRepo\\Chess\\Chess.txt", ref fileMessage);
+            FileRead("Chess.txt", ref fileMessage);
             string[] splitString = SplitString('\n', fileMessage);
+            DrawChessBoard(board);
             foreach (string line in splitString)
             {
-                ParseInput(args[0] + "\n");
+                ParseInput(line);
             }
         }
 
         static void FileRead(string path, ref string text)
         {
-            text = File.ReadAllText(path);
+            string fullpath = Path.GetFullPath(path);
+            text = File.ReadAllText(fullpath);
         }
 
         static string[] SplitString(char symbol, string textBeingSplit)
         {
             return textBeingSplit.Split(symbol);
+        }
+
+        enum ColumnCoordinates
+        {
+            A,
+            B,
+            C,
+            D,
+            E,
+            F,
+            G,
+            H
         }
 
         static void ParseInput(string toParse)
@@ -119,11 +136,16 @@ namespace Chess
         {
             char Column;
             int Row;
-            public ChessCoordinates(char column, int row)
+            ChessPiece piece;
+
+            public ChessCoordinates(char column, int row, ChessPiece piece = null) : this()
             {
                 Column = column;
                 Row = row;
+                Piece = piece;
             }
+
+            public ChessPiece Piece { get; set; }
 
             override
             public string ToString()
@@ -132,9 +154,48 @@ namespace Chess
             }
         }
 
-        static void DrawChessBoard()
+        static void DrawChessBoard(ChessCoordinates[,] coordinates)
         {
-
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write($"  {ColumnCoordinates.A + i}");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < coordinates.GetLength(0); i++)
+            {
+                Console.Write($"{1 + i}");
+                for (int j = 0; j < coordinates.GetLength(1); j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        if (coordinates[i, j].Piece == null && j % 2 == 0)
+                        {
+                            Console.Write(" - ");
+                        }
+                        else if (coordinates[i, j].Piece == null && j % 2 == 1)
+                        {
+                            Console.Write(" + ");
+                        }
+                    }
+                    else if (i % 2 == 1)
+                    {
+                        if (coordinates[i, j].Piece == null && j % 2 == 0)
+                        {
+                            Console.Write(" + ");
+                        }
+                        else if (coordinates[i, j].Piece == null && j % 2 == 1)
+                        {
+                            Console.Write(" - ");
+                        }
+                    }
+                }
+                Console.WriteLine($" {1 + i} ");
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write($"  {ColumnCoordinates.A + i}");
+            }
+            Console.WriteLine();
         }
 
 
