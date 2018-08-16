b37251cff61cd2a38d854ed1df20fa251856f41e
Alexander Quintanilla <xander.quin15@gmail.com>
2018-08-15 23:35:51 -0600

    UselessPush

--- ChessLibrary/Models/Queen.cs
+++ ChessLibrary/Models/Queen.cs
@@ -12,16 +12,10 @@ namespace ChessLibrary.Models
         public Queen()
         {
 
-<<<<<<< HEAD
         public bool IsLight { get; set; }
-=======
-<<<<<<< HEAD
         public new bool IsLight { get; set; }
-=======
         }
         public bool IsLight { get; set; }
->>>>>>> parent of c702ba9... Made more gui work :)
->>>>>>> 938db760a7d4a534b126bed48e4b50f420e19649
 
         public void Movement()
         {
